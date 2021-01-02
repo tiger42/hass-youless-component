21383a05bec0b0329d36d6b55d64a2b8312654cc
tiger42 <tiger42@gmxpro.de>
2021-01-02 16:51:29 +0100

    Added possibility to have more than one Youless device

--- custom_components/youless/sensor.py
+++ custom_components/youless/sensor.py
@@ -1,9 +1,9 @@
 """
-@ Author      : Gerben Jongerius, Paul de Wit, Robin Harmsen
-@ Date        : 04/29/2018, 11/01/2018, 04/29/2019
-@ Description : Youless Sensor - Monitor power consumption. 
+@ Author      : Gerben Jongerius, Paul de Wit, Robin Harmsen, Marc-Oliver Stühmer
+@ Date        : 04/29/2018, 11/01/2018, 04/29/2019, 01/02/2021
+@ Description : Youless Sensor - Monitor power consumption.
 """
-VERSION = '2.0.1'
+VERSION = '2.1.0'
 
 import json
 import logging
@@ -12,19 +12,23 @@ from urllib.request import urlopen
 
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
+
+from homeassistant.components.sensor import ENTITY_ID_FORMAT
 from homeassistant.const import CONF_MONITORED_VARIABLES
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import Entity, generate_entity_id
+from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.util import Throttle
 
 DOMAIN = 'youless'
-CONF_HOST = "host"
+CONF_HOST = 'host'
+CONF_NAME = 'name'
 
-SENSOR_PREFIX = 'youless_'
 _LOGGER = logging.getLogger(__name__)
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_NAME): cv.string,
         vol.Optional(CONF_MONITORED_VARIABLES, default=['pwr', 'net']): vol.All(
             cv.ensure_list, vol.Length(min=1), [vol.In(['pwr', 'net', 'p1', 'p2', 'n1', 'n2', 'cs0', 'ps0', 'gas'])])
     })
@@ -46,12 +50,14 @@ SENSOR_TYPES = {
 def setup_platform(hass, config, add_devices, discovery_info=None):
     host = config.get(CONF_HOST)
     sensors = config.get(CONF_MONITORED_VARIABLES)
+    name_prefix = config.get(CONF_NAME)
+
     data_bridge = YoulessDataBridge(host)
 
     devices = []
     for sensor in sensors:
         sensor_config = SENSOR_TYPES[sensor]
-        devices.append(YoulessSensor(data_bridge, sensor_config[0], sensor, sensor_config[1], sensor_config[2], sensor_config[3], sensor_config[4]))
+        devices.append(YoulessSensor(hass, data_bridge, name_prefix, sensor_config[0], sensor, sensor_config[1], sensor_config[2], sensor_config[3], sensor_config[4]))
 
     add_devices(devices)
 
@@ -73,15 +79,14 @@ class YoulessDataBridge(object):
 
 class YoulessSensor(Entity):
 
-    def __init__(self, data_bridge, name, prpt, sensor_id, uom, icon, image_uri):
+    def __init__(self, hass: HomeAssistantType, data_bridge, name_prefix, name, prpt, sensor_id, uom, icon, image_uri):
         self._state = None
         self._name = name
         self._property = prpt
         self._icon = icon
-        #self._image = image_uri
         self._uom = uom
         self._data_bridge = data_bridge
-        self.entity_id = 'sensor.' + SENSOR_PREFIX + sensor_id
+        self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, name_prefix + '_' + sensor_id, hass=hass)
         self._raw = None
 
     @property
@@ -91,10 +96,6 @@ class YoulessSensor(Entity):
     @property
     def icon(self):
         return self._icon
-
-    #@property
-    #def entity_picture(self):
-    #    return '/local/youless/' + self._image
 
     @property
     def unit_of_measurement(self):
